decb0a3df36f4bd5d371a431a291a63e81229261
mahdi <mkal1375@gmail.com>
2018-04-10 16:53:50 +0430

    A HUGE COMMIT! Add templating system(base.html , ....) Add some 'Admin' system code Add some urls patterns (like '','books',...) Add Some Views(index, books ,...) Add BookListView (class based view) Add some not important style and another things

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -5,15 +5,29 @@ from .models import Genre, Book, BookInstance, Author, Language
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     list_filter = ('genre',)
+    fields = [('title', 'author'), 'summery', ('isbn', 'language'), 'genre']
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstance(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Info', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': [('status', 'due_back')]
+        }),
+    )
 
 
 
